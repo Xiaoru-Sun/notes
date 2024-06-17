a66d9d1ec5a50c9009d8ce08a9c9859dccd99a71
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-17 23:52:28 +0100

    moves saveNote function to noteServices.ts

--- my-notes-app/components/CreateNotes.tsx
+++ my-notes-app/components/CreateNotes.tsx
@@ -1,14 +1,9 @@
 import React from "react";
 import { StyleSheet, Button, TextInput } from "react-native";
-import { useState } from "react";
 import { EditNotes } from "./EditNotes";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { saveNote } from "../services/noteServices";
 
 export const CreateNotes = () => {
-  const saveNote = async (input: string) => {
-    await AsyncStorage.setItem("note", input);
-  };
-
   return (
     <>
       <EditNotes saveNote={saveNote} />
--- my-notes-app/services/noteServices.ts
+++ my-notes-app/services/noteServices.ts
@@ -3,3 +3,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 export const getNote = async () => {
   return AsyncStorage.getItem("note");
 };
+
+export const saveNote = async (input: string) => {
+  await AsyncStorage.setItem("note", input);
+};
