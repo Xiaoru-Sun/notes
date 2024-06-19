39ba0af3b026fa85ac0161c51f07d529ef0d4881
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-19 10:16:10 +0100

    wrap the call back function inside useCallback

--- my-notes-app/components/NotesList.tsx
+++ my-notes-app/components/NotesList.tsx
@@ -1,20 +1,23 @@
 import React, { useState } from "react";
 import { Note, getAllNotes } from "../services/noteServices";
 import { useFocusEffect } from "@react-navigation/native";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
 
 export const NotesList = () => {
   const [noteList, setNoteList] = useState<Note[]>([]);
 
-  useFocusEffect(() => {
-    getAllNotes().then((result) => {
-      setNoteList(result.notes);
-    });
-  });
+  useFocusEffect(
+    React.useCallback(() => {
+      getAllNotes().then((result) => {
+        setNoteList(result.notes);
+      });
+    }, [noteList])
+  );
 
   return (
     <View>
       <Text>All recent notes</Text>
+
       {noteList.map((note, index) => (
         <Text key={index}>{note.text}</Text>
       ))}
