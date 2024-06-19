1cffd203c765be4b8b5dd7b0e3920ec865e047f1
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-19 11:39:05 +0100

    moves create new note button to header right

--- my-notes-app/App.tsx
+++ my-notes-app/App.tsx
@@ -1,7 +1,7 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet } from "react-native";
+import { StyleSheet, Button } from "react-native";
 import { Home } from "./components/Home";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { CreateNotes } from "./components/CreateNotes";
 import { RootStackParamsList } from "./types";
@@ -23,12 +23,24 @@ export default function App() {
         <Stack.Screen
           name="Home"
           component={Home}
-          options={{ title: "Home" }}
+          options={({ navigation }) => {
+            return {
+              title: "Home",
+              headerRight: () => (
+                <Button
+                  title="New Note"
+                  onPress={() => navigation.navigate("Notes")}
+                ></Button>
+              ),
+            };
+          }}
         ></Stack.Screen>
         <Stack.Screen
           name="Notes"
           component={CreateNotes}
-          options={{ title: "Edit Note" }}
+          options={{
+            title: "Edit Note",
+          }}
         ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
