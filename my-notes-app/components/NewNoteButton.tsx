89a3338ce6bf92b09eeca0a8510d2b686f082f11
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-19 12:02:46 +0100

    creates an individual component for this button

---
+++ my-notes-app/components/NewNoteButton.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { FontAwesome } from "@expo/vector-icons";
+import { Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { ScreenNavigationProp } from "../types";
+
+export const NewNoteButton = () => {
+  const navigation = useNavigation<ScreenNavigationProp>();
+  return (
+    <Pressable onPress={() => navigation.navigate("Notes")}>
+      <FontAwesome name="pencil-square-o" size={28}></FontAwesome>
+    </Pressable>
+  );
+};
