6f3faf9d6bcc332548efbc7219a3bbff1ff8c8f5
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-24 12:07:18 +0100

    adds RouteProp

--- my-notes-app/types.ts
+++ my-notes-app/types.ts
@@ -1,9 +1,12 @@
+import { RouteProp } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 
 export type RootStackParamsList = {
   Home: undefined;
-  Notes: undefined;
+  Notes: { noteId: string | undefined };
 };
 
 export type ScreenNavigationProp =
   NativeStackNavigationProp<RootStackParamsList>;
+
+export type EditScreenRouteProp = RouteProp<RootStackParamsList, "Notes">;
