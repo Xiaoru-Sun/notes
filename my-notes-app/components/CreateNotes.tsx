54371250238872f8e34a2dae91f4107594fb2e5c
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-24 22:51:17 +0100

    adds route.params in to pass it to EitNotes component

--- my-notes-app/components/CreateNotes.tsx
+++ my-notes-app/components/CreateNotes.tsx
@@ -2,11 +2,15 @@ import React from "react";
 import { StyleSheet, Button, TextInput } from "react-native";
 import { EditNotes } from "./EditNotes";
 import { saveNote } from "../services/noteServices";
+import { useRoute } from "@react-navigation/native";
+import { EditScreenRouteProp } from "../types";
 
 export const CreateNotes = () => {
+  const route = useRoute<EditScreenRouteProp>();
+  const noteId = route.params.noteId;
   return (
     <>
-      <EditNotes saveNote={saveNote} />
+      <EditNotes saveNote={saveNote} noteId={noteId} />
     </>
   );
 };
