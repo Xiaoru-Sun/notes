dff6b5aaa13a6705ec60efca84924e6f179f60b7
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-25 13:23:31 +0100

    adds delete button on saved notes

--- my-notes-app/components/OpenNotes.tsx
+++ my-notes-app/components/OpenNotes.tsx
@@ -1,11 +1,21 @@
-import React from "react";
-import { EditNotes } from "./EditNotes";
-import { useRoute } from "@react-navigation/native";
-import { EditScreenRouteProp } from "../types";
+import React, { useLayoutEffect } from "react";
+import { EditNotes } from "./EditNotesScreen";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { EditScreenRouteProp, ScreenNavigationProp } from "../types";
+import { Button } from "react-native";
 
 export const OpenNotes = () => {
   const route = useRoute<EditScreenRouteProp>();
+  const navigation = useNavigation<ScreenNavigationProp>();
   const noteId = route.params.noteId;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: noteId ? "Edit Note" : "New Note",
+      headerRight: () => (noteId ? <Button title="Delete"></Button> : <></>),
+    });
+  }, [navigation]);
+
   return (
     <>
       <EditNotes noteId={noteId} />
