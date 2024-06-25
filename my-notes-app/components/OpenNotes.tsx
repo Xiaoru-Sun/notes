0528b619e998852f564f1eadefe057dea6b36e04
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-25 12:50:27 +0100

    renames component CreateNotes to OpenNotes as this component is used for both new notes and save notes

---
+++ my-notes-app/components/OpenNotes.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { EditNotes } from "./EditNotes";
+import { useRoute } from "@react-navigation/native";
+import { EditScreenRouteProp } from "../types";
+
+export const OpenNotes = () => {
+  const route = useRoute<EditScreenRouteProp>();
+  const noteId = route.params.noteId;
+  return (
+    <>
+      <EditNotes noteId={noteId} />
+    </>
+  );
+};
