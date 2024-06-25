b230eaed5661c7a41dc2fa647207ef00434ebae1
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-06-25 12:34:47 +0100

    imports route

--- my-notes-app/components/CreateNotes.tsx
+++ my-notes-app/components/CreateNotes.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { StyleSheet, Button, TextInput } from "react-native";
 import { EditNotes } from "./EditNotes";
-import { saveNote } from "../services/noteServices";
 import { useRoute } from "@react-navigation/native";
 import { EditScreenRouteProp } from "../types";
 
@@ -10,7 +8,7 @@ export const CreateNotes = () => {
   const noteId = route.params.noteId;
   return (
     <>
-      <EditNotes saveNote={saveNote} noteId={noteId} />
+      <EditNotes noteId={noteId} />
     </>
   );
 };
